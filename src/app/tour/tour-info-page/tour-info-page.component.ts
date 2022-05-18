7a13153301b79fb1bb2b61f29cde3c472eb089db
Andriy Kotiyga <kotygaandrey05@gmail.com>
2022-05-18 23:32:13 +0300

    - completed working with images

--- src/app/tour/tour-info-page/tour-info-page.component.ts
+++ src/app/tour/tour-info-page/tour-info-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from "@angular/router";
-import {TourService} from "../../_services/tour.service";
-import {Tour} from "../../_models/tour.model";
+import { TourService } from "../../_services/tour.service";
+import { Tour } from "../../_models/tour.model";
 
 @Component({
   selector: 'app-tour-info-page',
@@ -12,24 +12,23 @@ export class TourInfoPageComponent implements OnInit {
 
   tourId: number;
   tour: Tour;
-  urlsToSlider = [];
+  carouselWidth: number;
 
   constructor(private router: Router,
               private tourService: TourService,
-              private activatedRoute: ActivatedRoute) { }
+              private activatedRoute: ActivatedRoute) {
+  }
 
   ngOnInit(): void {
     this.tourId = this.activatedRoute.snapshot.params['id'];
     this.tourService.getById$(this.tourId).subscribe(response => {
       this.tour = Tour.fromObject(response);
-      this.urlsToSlider =  this.tour?.urls;
+      this.carouselWidth = this.tour.urls?.length > 2 ? 920 : 610;
     });
   }
 
   edit(tourId: number): void {
     this.router.navigate(['tour', 'edit', tourId]);
-    // this.router.navigateByUrl('tour/edit/' + tourId);
-    console.log('tourId >> ', tourId);
   }
 
 }
